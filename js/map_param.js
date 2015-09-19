a431716d5b279d4de13395bf1551c497434328d6
ida <idasarahchow@gmail.com>
2015-09-19 13:41:55 -0400

    standardize inputs and more responsive

--- js/map_param.js
+++ js/map_param.js
@@ -1,7 +1,7 @@
 function submitPoints() {
     console.log("hi dweebteam");
     var startPoint = document.getElementById('startPoint').value;
-    var endTime = document.getElementById('endPoint').value;
+    var endPoint = document.getElementById('endPoint').value;
     localStorage.setItem("start", startPoint);
     localStorage.setItem("end", endPoint);
     console.log(localStorage.getItem("start"));
@@ -9,7 +9,17 @@ function submitPoints() {
     return true;
 }
 
+
 function loadFunction() {
     console.log(localStorage.getItem("start"));
-    document.getElementById("initial-start").innerHTML = localStorage.getItem("start");   
+    
+//    placeholder for start and end points
+    document.getElementById("initial-start").innerHTML = "<div class='input-group'><span class='input-group-addon' id='basic-addon1'>start</span><input type='text' id='startparam' class='form-control' placeholder='" +
+        localStorage.getItem("start") + 
+        "' aria-describedby='basic-addon1'></div>"
+    
+    document.getElementById("initial-end").innerHTML = "<div class='input-group'><span class='input-group-addon' id='basic-addon1'>end</span><input type='text' id='startparam' class='form-control' placeholder='" +
+    localStorage.getItem("end") + 
+    "' aria-describedby='basic-addon1'></div>"
+
 }
